93a25d67d423e063040d7d62367a0d6fa08ebffc
tncvasconcelos <thais.nogales@gmail.com>
2020-08-04 17:58:08 -0500

    adding first files

---
+++ neotropical_climbers_script.R
@@ -0,0 +1,56 @@
+# Diversification of Neotropical climbers across space and time
+
+library(data.table)
+library(monographaR)
+library(tidyverse)
+library(raster)
+library(CoordinateCleaner)
+library(ape)
+source("~/Desktop/climbers/neotropical_climbers_functions.R")
+
+# First get grid cell values from filtered GBIF data 
+####
+#### from TV local
+gbif_dir <- "~/Desktop/full_gbif_quest/" 
+climbers_dir <- "~/Desktop/climbers"
+gbif_files <- list.files(paste0(gbif_dir, "z_filtered_gbif"), ".csv")
+full_list <- read.full.gbif(gbif_files, gbif_dir)
+write.csv(full_list, file=paste0(gbif_dir, "full_tracheophyte_filtered_gbif.csv"))
+
+# load table back
+setwd()
+full_list <- fread(paste0(gbif_dir, "full_tracheophyte_filtered_gbif.csv"))
+full_list <- full_list[,-1]
+
+setwd(climbers_dir)
+full_map <- run.mapDiversity.neotropics(full_list)
+
+pdf("all_angio_div.pdf", height=8, width=5)
+plot(full_map)
+dev.off()
+
+# load table with climbers
+
+climbers <- read.csv("climber_database.csv", stringsAsFactors = F)
+
+# Make map of grid cell diversity for each of the 8 mechanisms
+for(mechanism_index in 1:8) {
+  # Select mechanism
+  mech <- climbers$Species[climbers$CM==mechanism_index]
+  # Taxize names
+  mech_taxized <- gbif.taxize(mech)
+  # Get points from full list
+  mech_points <- subset(full_list, full_list$V1 %in% as.character(mech_taxized))
+  # Get raster of grid cells 
+  mech_map <- run.mapDiversity.neotropics(mech_points, filename=paste0("Mechanism_", mechanism_index))
+  # Plot pdf
+  pdf(paste0("Mechanism_", mechanism_index,".pdf"), height=8, width=5)
+  plot(mech_map)
+  dev.off()
+}
+
+# Load raster back
+full_map <- readRDS("full_neotropical_diversity.Rdata")
+mechanism1 <- readRDS("Mechanism_1.Rdata")
+
+
