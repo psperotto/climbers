62728c31545e12c6f08370f5569a97e59144f679
tncvasconcelos <thais.nogales@gmail.com>
2021-11-29 09:27:39 -0600

    taxizing larger tree

--- 2021-11-25_taxize-tree.R
+++ 2021-11-25_taxize-tree.R
@@ -3,7 +3,7 @@ library(ape)
 source("neotropical_climbers_functions.R") 
 
 # Taxizing tree
-sb_tree <- read.tree("Data/GBMB.tre")
+sb_tree <- read.tree("Data/ALLOTB.tre")
 sb_tree_taxized_tips <- gbif.taxize(sb_tree$tip.label)
 sb_tree$tip.label <- sb_tree_taxized_tips
-saveRDS(sb_tree, file="Data/taxized_GBMB.Rdata")
+saveRDS(sb_tree, file="Data/taxized_ALLOTB.Rdata")
