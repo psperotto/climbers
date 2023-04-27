7ba69d3e82a1887b55c6837866ec813a4dc82fc9
Thais Vasconcelos <tvasc@eeb-v32jt77k43.local>
2023-04-27 10:28:33 -0400

    organizing repo

--- others/07_map_for_intro.R
+++
@@ -1,3 +0,0 @@
-# Map for introduction
-
-
--- others/2021-11-25_taxize-tree.R
+++
@@ -1,9 +0,0 @@
-rm(list=ls())
-library(ape)
-source("neotropical_climbers_functions.R") 
-
-# Taxizing tree
-sb_tree <- read.tree("Data/ALLOTB.tre")
-sb_tree_taxized_tips <- gbif.taxize(sb_tree$tip.label)
-sb_tree$tip.label <- sb_tree_taxized_tips
-saveRDS(sb_tree, file="Data/taxized_ALLOTB.Rdata")
